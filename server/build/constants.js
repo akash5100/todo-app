ecb2742c50e08ba29d3f10db3bb9874d9273f093
akash5100 <shivamvermashs@gmail.com>
2022-12-11 12:58:09 +0530

    purge build and add ignore to db.sqlite

--- server/build/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.port = void 0;
-// Constant for the app, example port
-const port = 5000;
-exports.port = port;
